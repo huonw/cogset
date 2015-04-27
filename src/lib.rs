01e8953e1ebd08c826469778285c4880e4bc5d65
Huon Wilson <dbau.pp+github@gmail.com>
2015-04-27 15:33:35 +1000

    Improve doc landing page.

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,15 @@
 //! Clustering algorithms.
 //!
+//! ![A cluster](http://upload.wikimedia.org/wikipedia/commons/e/e1/Cassetteandfreehub.jpg)
+//!
 //! This crate provides generic implementations of clustering
 //! algorithms, allowing them to work with any back-end "point
 //! database" that implements the required operations, e.g. one might
 //! be happy with using the naive collection `BruteScan` from this
 //! crate, or go all out and implement a specialised R*-tree for
 //! optimised performance.
+//!
+//! `Dbscan` offers density-based clustering via the DBSCAN algorithm.
 //!
 //! [Source](https://github.com/huonw/cogset).
 //!
